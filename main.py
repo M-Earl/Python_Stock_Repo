384c1dab0b97216c58d27791dbb94acce1d4b853
Mike Earl <mearl473@gmail.com>
2022-09-14 22:16:32 -0400

    Added an example label

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter as tk
 def main():
     print("Python Running...")
     window = tk.Tk()
+    voo_value = tk.Label(text="Value of VOO: ABC.XX")
+    voo_value.pack()
     window.mainloop()
 
 if __name__ == "__main__":
