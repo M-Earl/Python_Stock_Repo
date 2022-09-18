ee5b72b417555dcabee4bb32f215517c811cd3ac
Mike Earl <mearl473@gmail.com>
2022-09-17 22:33:19 -0400

    Add basic example of request handling

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import tkinter as tk
+import requests
+
+url = "https://api.weather.gov/points/39.7456,-97.0892"
 
 def main():
     print("Python Running...")
+
+    response = requests.get(url)
+    print(response.json())
+
     window = tk.Tk()
     voo_value = "ABC.XX"
     voo_value_label = tk.Label(text="Value of VOO: " + voo_value)
