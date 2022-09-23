ef09ca48172815a9e2cc6846f93fb49e748f6402
Mike Earl <mearl473@gmail.com>
2022-09-22 23:03:56 -0400

    Added error handling in request

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ port = "3000"
 def main():
     print("Python Running...")
 
-    response = requests.get(local_host + ":" + port)
-    print(response.json())
+    voo_value = "Undefined"
+
+    try:
+        response = requests.get(local_host + ":" + port)
+        voo_value = str(response.json())
+    except BaseException as error:
+        print("Error occurred with receiving a response")
 
     window = tk.Tk()
-    voo_value_label = tk.Label(text="Value of VOO: " + str(response.json()))
+    voo_value_label = tk.Label(text="Value of VOO: " + str(voo_value))
     voo_value_label.pack(padx = 10, pady = 10)
     window.mainloop()
 
